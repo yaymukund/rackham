972d59a9d8051d6842aec207828a99ddeafa1a20
Mukund Lakshman <yaymukund@gmail.com>
2014-04-12 17:09:37 -0400

    Fix build script to corrrectly cp images

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
-APP_PUBLIC_PATH="../music-room/public/assets";
+APP_PUBLIC_PATH="../music-room/public";
 
 node_modules/ember-cli/bin/ember build --env $1
 echo 'Compiled to dist/assets/';
 
 mkdir -p ${APP_PUBLIC_PATH}
-cp dist/assets/env.js "${APP_PUBLIC_PATH}";
-cp dist/assets/app.js "${APP_PUBLIC_PATH}";
-cp dist/assets/app.css "${APP_PUBLIC_PATH}";
+cp dist/assets/env.js "${APP_PUBLIC_PATH}/assets";
+cp dist/assets/app.js "${APP_PUBLIC_PATH}/assets";
+cp dist/assets/app.css "${APP_PUBLIC_PATH}/assets";
+cp -r dist/images "${APP_PUBLIC_PATH}";
 echo "Copied env.js, app.js, app.css to ${APP_PUBLIC_PATH}";
