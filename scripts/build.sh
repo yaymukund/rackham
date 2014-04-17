ab281aa29dd3413120ce62f3e29a7ad503db3a0b
Mukund Lakshman <yaymukund@gmail.com>
2014-04-17 02:15:14 -0400

    Also copy mobile.css when building.

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,6 @@ mkdir -p ${APP_PUBLIC_PATH}
 cp dist/assets/env.js "${APP_PUBLIC_PATH}/assets";
 cp dist/assets/app.js "${APP_PUBLIC_PATH}/assets";
 cp dist/assets/app.css "${APP_PUBLIC_PATH}/assets";
+cp dist/assets/mobile.css "${APP_PUBLIC_PATH}/assets";
 cp -r dist/images "${APP_PUBLIC_PATH}";
-echo "Copied env.js, app.js, app.css to ${APP_PUBLIC_PATH}";
+echo "Copied env.js, app.js, app.css, movile.css to ${APP_PUBLIC_PATH}";
