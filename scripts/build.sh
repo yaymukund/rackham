baf29ab0d09bb8f4eb62534836fedd32bac2c5cb
Mukund Lakshman <yaymukund@gmail.com>
2014-04-30 11:34:53 -0400

    Fixed all-imporatnt typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,4 @@ cp dist/assets/app.js "${APP_PUBLIC_PATH}/assets";
 cp dist/assets/app.css "${APP_PUBLIC_PATH}/assets";
 cp dist/assets/mobile.css "${APP_PUBLIC_PATH}/assets";
 cp -r dist/images "${APP_PUBLIC_PATH}";
-echo "Copied env.js, app.js, app.css, movile.css to ${APP_PUBLIC_PATH}";
+echo "Copied env.js, app.js, app.css, mobile.css to ${APP_PUBLIC_PATH}";
